8c95e21d510dfdc5c30a1f7ed777655fd7ae7668
carltonupperdine <carlton.upperdine@answerdigital.com>
2022-05-16 09:30:10 +0100

    scaffold tasks page

---
+++ src/app/tasks/tasks.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TasksComponent } from './tasks.component';
+
+@NgModule({
+  declarations: [TasksComponent],
+  imports: [CommonModule]
+})
+export class TasksModule {}
