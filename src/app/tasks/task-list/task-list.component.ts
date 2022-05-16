743920d9e701cdb6009d45354b93e46bf4cb666f
carltonupperdine <carlton.upperdine@answerdigital.com>
2022-05-16 17:13:35 +0100

    add basic color coding

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -9,4 +9,24 @@ import { Task } from '../task.model';
 export class TaskListComponent {
   @Input() tasks: Task[] = [];
   columnsToDisplay = ['complete', 'id', 'title', 'description', 'due'];
+
+  getColour(task: Task): string {
+    if (!task.due) return '';
+
+    if (task.complete) return 'completed';
+
+    const overdue = task.due < new Date();
+    if (overdue) return 'overdue';
+
+    const ONE_DAY = 1000 * 60 * 60 * 24;
+    const daysRemaining =
+      Math.floor(task.due.getMilliseconds() - new Date().getMilliseconds()) /
+      ONE_DAY;
+
+    if (daysRemaining < 7) {
+      return 'due-soon';
+    }
+
+    return '';
+  }
 }
