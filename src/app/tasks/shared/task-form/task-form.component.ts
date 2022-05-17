2126dbe5ec513e4eb6446380aa027aae789b7ce4
carltonupperdine <carlton.upperdine@answerdigital.com>
2022-05-17 12:02:41 +0100

    refactor task form to use reactive forms

--- src/app/tasks/shared/task-form/task-form.component.ts
+++ src/app/tasks/shared/task-form/task-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { EMPTY_TASK } from '../../constants';
 import { EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Task } from '../../task.model';
 
 @Component({
@@ -8,8 +9,26 @@ import { Task } from '../../task.model';
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.scss']
 })
-export class TaskFormComponent {
+export class TaskFormComponent implements OnInit {
   @Input() action: string = 'Create';
   @Input() task: Task = EMPTY_TASK;
   @Output() submitted = new EventEmitter<Task>();
+
+  title = new FormControl('');
+  description = new FormControl('');
+  due = new FormControl();
+
+  submit() {
+    this.task.title = this.title.value;
+    this.task.description = this.description.value;
+    this.task.due = this.due.value;
+    this.submitted.emit(this.task);
+  }
+
+  ngOnInit(): void {
+    if (!this.task) return;
+    this.title.setValue(this.task.title);
+    this.description.setValue(this.task.description);
+    this.due.setValue(this.task.due);
+  }
 }
