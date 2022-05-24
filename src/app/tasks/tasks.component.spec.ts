aa7d1a951300b9473694a375d353bf2037d7e444
carltonupperdine <carlton.upperdine@answerdigital.com>
2022-05-24 15:50:55 +0100

    sort tasks in ID order

--- src/app/tasks/tasks.component.spec.ts
+++ src/app/tasks/tasks.component.spec.ts
@@ -1,25 +1,20 @@
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngrx/store';
 import { TaskFacade } from '../store/tasks/task.facade';
 import { TasksComponent } from './tasks.component';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 describe('TasksComponent', () => {
   let component: TasksComponent;
 
   beforeEach(async () => {
-    const facadeMock = jest.mock<TaskFacade>('../store/tasks/task.facade');
-
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [TasksComponent],
-      providers: [
-        provideMockStore(),
-        MockStore,
-        TasksComponent,
-        { provide: TaskFacade, useValue: facadeMock }
-      ]
+      providers: [provideMockStore(), TaskFacade, MockStore, TasksComponent]
     }).compileComponents();
   });
 
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy } from '@angular/core';
-
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TaskFacade } from '../store/tasks';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
@@ -9,7 +9,9 @@ import { TaskFacade } from '../store/tasks';
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent {
-  tasks$ = this.facade.tasks$;
+  tasks$ = this.facade.tasks$.pipe(
+    map((tasks) => tasks.sort((a, b) => a.id - b.id))
+  );
 
   constructor(public router: Router, private facade: TaskFacade) {}
 
